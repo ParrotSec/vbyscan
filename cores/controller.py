d5c31214f56cb2ad5139bedbbf4ccb16706329f6
Nong Hoang Tu <dmknght@parrotsec.org>
2021-04-02 16:40:16 +0700

    Use traceback for exceptions

--- cores/controller.py
+++ cores/controller.py
@@ -4,9 +4,15 @@ import requests
 
 def fingerprint(client, target, verbose_cb, found_cb, not_found_cb):
     from modules.info import version
-    from modules.info import firewall
     version.get_version(client, target, verbose_cb, found_cb, not_found_cb)
-    firewall.firewall_detector(client, target, verbose_cb, found_cb, not_found_cb)
+    # FIXME firewall is so slow
+    # from modules.info import firewall
+    # firewall.firewall_detector(client, target, verbose_cb, found_cb, not_found_cb)
+    # TODO check robots.txt
+    # TODO check validator.php
+    # TODO check admin control panel and moderator control panel
+    # TODO: check apache status
+    # TODO: check backup and log
 
 
 def vuln_scan(client, target, verbose_cb, found_cb, not_found_cb):
@@ -15,9 +21,11 @@ def vuln_scan(client, target, verbose_cb, found_cb, not_found_cb):
     for module_name in cores.exploit_modules():
         module = importlib.import_module("modules.exploits." + module_name)
         try:
+            import traceback
             module.run(client, target, verbose_cb, found_cb, not_found_cb)
         except Exception as error:
-            print("Runtime error: " + error)
+            print("Runtime error")
+            traceback.print_exc()
 
 
 def main_logic(target, verbose=True):
