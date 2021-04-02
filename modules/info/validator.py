ec23ba7fc17be9c54eb05413e7884657ecaa70ba
Nong Hoang Tu <dmknght@parrotsec.org>
2021-04-02 16:52:01 +0700

    Add validator disclosure

---
+++ modules/info/validator.py
@@ -0,0 +1,15 @@
+def run(req, target, info_cb, found_cb, not_found_cb):
+    # https://github.com/OWASP/vbscan/blob/master/modules/validator.pl
+    name = "validator.php information disclosure"
+    uri = f"{target}validator.php"
+    is_vulnerable = False
+
+    info_cb(f"Checking {name}")
+    r = req.get(uri)
+
+    if r.status_code == 200 and "validate" in str(r.content):
+        found_cb(name, uri)
+        is_vulnerable = True
+
+    if not is_vulnerable:
+        not_found_cb(name)
