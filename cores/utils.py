f984bbf9a0b5c1063aa57892728006ce5eef239e
Nong Hoang Tu <dmknght@parrotsec.org>
2021-04-02 16:15:05 +0700

    Add program banner

--- cores/utils.py
+++ cores/utils.py
@@ -28,3 +28,17 @@ def print_not_found(message, uri = ""):
     print(f" [!] {message} not found")
     if uri:
         print(f"  {uri}")
+
+
+def help_banner():
+    pass
+
+
+def program_banner():
+    print("      ___      ___               ")
+    print(" __ _| _ )_  _/ __| __ __ _ _ _  ")
+    print(" \\ V / _ \\ || \\__ \\/ _/ _` | ' \\ ")
+    print("  \\_/|___/\\_, |___/\\__\\__,_|_||_|")
+    print("          |__/                   ")
+    print("vByScan - vBulletin scanner")
+    print("This program is a fork of vbscan")
