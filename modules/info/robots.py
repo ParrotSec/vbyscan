e0e4960cad94b74d271c2a98bb415b75ae114b8a
Nong Hoang Tu <dmknght@parrotsec.org>
2021-04-04 00:55:23 +0700

    Add filter for \r and \n

--- modules/info/robots.py
+++ modules/info/robots.py
@@ -10,7 +10,7 @@ def robot_check(req, target, info_cb, not_found_cb):
         if r.status_code == 200 and "text/plain" in r.headers["Content-Type"]:
             for line in r.content.decode('utf-8').split("\n"):
                 if line.startswith("Disallow:"):
-                    branch = line.split(": /")[1]
+                    branch = line.split(": /")[1].replace("\r", "").replace("\n", "")
                     new_uri = f"{target}{branch}"
                     sr = req.get(new_uri)
                     # TODO custom color here
