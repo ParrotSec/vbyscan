52585b93224733ddcf5259f3036b55359df23fb0
Nong Hoang Tu <dmknght@parrotsec.org>
2021-04-04 14:06:42 +0700

    Add simple testcase for db_cmp

---
+++ test/test_db_ver_cmp.py
@@ -0,0 +1,43 @@
+# Test compare version
+
+from cores import db_handler
+
+version = "4.1.1"
+
+print("Test compare match range")
+db_versions = [
+    ("4.1.0-4.1.3", True),
+    ("4.1.6-4.1.7", False),
+    ("4.0.2-4.3.x", True),
+    ("-4.10.7", True),
+    ("4.1.0-", True),
+    ("3.0.x-5.x", True),
+    ("4.1.1-4.5.9", True),
+    ("4.0.2-4.1.1", True)
+]
+
+for db_version in db_versions:
+    print(f" {version}: {db_version[0]}")
+    result = db_handler.match_range(version, db_version[0])
+    print(f"  Expected: {db_version[1]} | Result: {result}")
+    if result != db_version[1]:
+        print(f"   Test failed for {db_version[0]}")
+
+
+db_versions = [
+    ("4.1.0<4.1.3", True),
+    ("4.1.6<4.1.7", False),
+    ("4.0.2<4.3.x", True),
+    ("<4.10.7", True),
+    ("4.1.0<", True),
+    ("3.0.x<5.x", True),
+    ("4.1.0<4.1.1", False),
+    ("4.1.1<4.1.3", True),
+]
+
+for db_version in db_versions:
+    print(f" {version}: {db_version[0]}")
+    result = db_handler.match_lesser(version, db_version[0])
+    print(f"  Expected: {db_version[1]} | Result: {result}")
+    if result != db_version[1]:
+        print(f"   Test failed for {db_version[0]}")
