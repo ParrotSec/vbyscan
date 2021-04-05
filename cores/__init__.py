d4c2a3ffef0b57a5a2d83990f16e784c932b2574
Nong Hoang Tu <dmknght@parrotsec.org>
2021-04-05 15:07:19 +0700

    Improve print and improve program banner

--- cores/__init__.py
+++ cores/__init__.py
@@ -39,7 +39,7 @@ def enum_vuln(version):
         vuln_db = json.load(vuln_db_file)
         for each_vuln in vuln_db['vuln_db']:
             if db_handler.version_match(version, each_vuln['version']):
-                print(f"  [*] \033[97m{each_vuln['id']}\033[00m - \033[95m{each_vuln['name']}\033[00m")
-                print(f"      \033[94m{each_vuln['link']}\033[00m")
+                print(f"  [*] \033[97m{each_vuln['id']}\033[0m - \033[95m{each_vuln['name']}\033[0m")
+                print(f"      \033[94m{each_vuln['link']}\033[0m")
                 if each_vuln['exploit']:
-                    print(f"      \033[91m{each_vuln['exploit']}\033[00m")
+                    print(f"      \033[91m{each_vuln['exploit']}\033[0m")
--- cores/utils.py
+++ cores/utils.py
@@ -8,7 +8,7 @@ def print_verbose(message):
 
 def print_vulnerable(name, uri=""):
     # Bright Magenta
-    print(f"  [\033[96m*\033[00m] \033[95m{name}\033[00m is\033[91m vulnerable\033[00m")
+    print(f"  [\033[96m*\033[0m] \033[95m{name}\033[0m is\033[91m vulnerable\033[0m")
     if uri:
         # Bright Cyan
         print(f"  \033[96m{uri}\033[0m")
@@ -16,19 +16,19 @@ def print_vulnerable(name, uri=""):
 
 def print_not_vulnerable(name):
     # Bright yellow
-    print(f"  [\033[93m!\033[00m] \033[93m{name}\033[00m is\033[37m not vulnerable\033[00m")
+    print(f"  [\033[93m!\033[0m] \033[93m{name}\033[0m is\033[37m not vulnerable\033[0m")
 
 
 def print_found(message, uri=""):
     # Bright white
-    print(f"  [\033[97m*\033[00m] \033[97m{message}\033[00m")
+    print(f"  [\033[97m*\033[0m] \033[97m{message}\033[0m")
     if uri:
         # Bright blue
-        print(f"  \033[94m{uri}\033[00m")
+        print(f"  \033[94m{uri}\033[0m")
 
 
 def print_not_found(message):
-    print(f"  [\033[91m-\033[00m] {message} not found")
+    print(f"  [\033[91m-\033[0m] {message} not found")
 
 
 def help_banner(program):
@@ -36,10 +36,15 @@ def help_banner(program):
 
 
 def program_banner():
+    import cores
     print("      ___      ___               ")
     print(" __ _| _ )_  _/ __| __ __ _ _ _  ")
     print(" \\ V / _ \\ || \\__ \\/ _/ _` | ' \\ ")
     print("  \\_/|___/\\_, |___/\\__\\__,_|_||_|")
     print("          |__/                   \n")
-    print("vByScan - vBulletin scanner of Parrot OS")
-    print("This program is a fork of vbscan\n")
+    print("-----[ \033[95mvBulletin Scanner\033[0m ]---[ ParrotOS Team ]")
+    print(f"-----[ Version: \033[97m{cores.get_version()}\033[0m ]")
+    print("-----[ Author: \033[96mNông Hoàng Tú\033[0m ]---[ \033[94mdmknght@parrotsec.org\033[0m ]")
+    print("-----[ License: \033[93mGPL-3\033[0m ]")
+    print("-----[ \033[94mhttps://nest.parrotsec.org/packages/tools/vbyscan/\033[0m ]")
+    print("This program is a fork of OWASP_VBScan in python3\n")
