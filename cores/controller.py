2e7df8b24247638e031979c732cfd35d24b1f813
Nong Hoang Tu <dmknght@parrotsec.org>
2021-04-05 18:09:11 +0700

    Move password_logger to enumerate

--- cores/controller.py
+++ cores/controller.py
@@ -30,7 +30,7 @@ def fingerprint(client, target, version, verbose_cb, found_cb, not_found_cb):
     finder.error_finder(client, target, verbose_cb, found_cb, not_found_cb)
     finder.backup_finder(client, target, verbose_cb, found_cb, not_found_cb)
 
-    from modules.info import pass_logger
+    from modules.enumerate import pass_logger
     pass_logger.check_password_logger(client, target, verbose_cb, found_cb, not_found_cb)
 
 
